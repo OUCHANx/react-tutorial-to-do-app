8e1857084ca8a6bf758d50f275c746cab0e5c659
ouchan <kingbook0628@gmail.com>
2026-03-08 20:53:16 +0900

    uuidについて学ぶ

---
+++ src/Todo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Todo = ({ todo }) => {
+  return <div>
+    <input type = "checkbox" checked = {todo.completed} readOnly/>
+    {todo.name}</div>;
+}
+export default Todo;
